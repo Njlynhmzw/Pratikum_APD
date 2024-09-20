86a63f796ef365b9a7c1060fa2ee5855c36031b8
Njlynhmzw <julyanahnou@gmail.com>
2024-09-20 13:55:51 +0700

    materi pertemuan 3

---
+++ pertemuan-3/pertemuan-3.py
@@ -0,0 +1,35 @@
+
+uang = "True"
+uang = 2000
+if uang: 
+    print("pergi ke pasar")
+else:
+    print("pergi ke rumah teman")
+
+nilai = -1
+if nilai < 70:
+    print("tidak lulus")
+else
+if nilai < 0:
+    print ("bilangan negatif")
+else:
+    print("bilangan positif")
+"umur"= int(input("masukkan umur anda:"))
+if umur <= 10:
+    print("umur termasuk kategori anak-anak")
+elif umur <= 18:
+    print("umur anda termasuk kategori remaja")
+elif umur <= 35:
+    print("dewasa")
+elif umur <= 65:
+    print("paruh baya")
+else:
+    print("tua")
+nilai = 79
+if >= 80:
+    if nilai >= 85: 
+        print("nilai A+")
+    else :
+        print ("nllai A-")
+else : 
+    print ("tidak lulus")
