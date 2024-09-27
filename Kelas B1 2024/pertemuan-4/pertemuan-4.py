88218dca8dce6286ff05595c9e0c22251c2d1f2b
Njlynhmzw <julyanahnou@gmail.com>
2024-09-27 14:03:33 +0700

    Add files via upload

---
+++ Kelas B1 2024/pertemuan-4/pertemuan-4.py
@@ -0,0 +1,48 @@
+#batas = 20
+#for i in range(1, batas, 2):
+#    print(f"perulangan ke-{i}") 
+#buah_banyak = ("apel", "mangga", "anggur")
+#for buah in buah_banyak:
+#    print(buah)
+# for baris in range(1,4):
+#     print("baris ke", baris)
+#     for kolom in range(1,4):
+#         print(kolom, "kolom", end="")
+#     print()
+#     print()
+# while "hello" :
+#     print("hello")
+# 
+# for i in range(1,10):
+#     if i == 4:
+#         break 
+#     else:
+#         print(f"perulangan ke -{i}")
+# while true:
+#     ulang = input ("mabar lagi : ")
+#     if ulang == "gk" : 
+#         break
+#     print("mabar lagi")
+# for i in range(1,10):
+#     if i % 2 == 1:
+#         continue
+#     print(f"perulangan ke-{i}")
+# print("segitiga siku-siku")
+# for i in range(1,5):
+#     print("*" * i)
+# kesempatan = 3
+# while kesempatan > 0:
+#     username = input("masukkan username: ")
+#     password = input("masukkan password: ")
+
+#     if username == "admin" and password == "admin#1234":
+#         print("berhasil login")
+#         break
+#     else:
+#         print("username atau password salah!")
+#         kesempatan -= 1
+#         print(f"kesempatan tersisa ke-{kesempatan}")
+
+#     print("program utama")
+
+
