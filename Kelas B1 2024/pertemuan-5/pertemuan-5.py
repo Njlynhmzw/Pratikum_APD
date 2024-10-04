3a23d5681da76e62f862cd392711b3e6c7983b6b
Njlynhmzw <julyanahnou@gmail.com>
2024-10-04 14:15:36 +0700

    Add files via upload

---
+++ Kelas B1 2024/pertemuan-5/pertemuan-5.py
@@ -0,0 +1,44 @@
+# data = [1,2,3,4,True,[2,3], "adi"]
+# data_2 = list((1,2,3,4,5))
+
+# print(data[6])
+ # print(type(data_2))
+# data = ["apel", "durian", "jerukk"]
+# print(data)
+# print()
+# data[0]= "anggur"
+# print(data)
+# data.extend({"rambutan", "semangaka"})
+# data.append("semangka")
+# data.append(True)
+# print(data)
+# data.insert(0,"rambutan")
+# print(data)
+# data = ["apel", "durian", "jerukk"]
+# print(data*3)
+# print("="*3)
+# del data [0]
+# nilai_hapus = data.pop(1)
+# data.remove("apel")
+
+# print(nilai_hapus)
+# data = ["apel", "durian", "jerukk"]
+# print(data)
+# print()
+# data_mahasiswa= [
+#     ["admin", "admin123", "admin"],
+#     ["user","user1234","user"]
+# ]
+
+# print(data_mahasiswa[0][2][0])
+# buah = ["apel", "durian", "jeruk"]
+# index = 1
+# for data in buah :
+#     print(f "data ke {index}")
+#     print(data)
+#     print("="*10)
+#     index = index + 1
+# for index, item in enumerate(data_mahasiswa) :
+#     print(f"data ke {index+1}")
+# data_mahasiswa = ["ifnu", "adi", "ucup", ]
+    
